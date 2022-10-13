b638092cada8a412e287207b41ab8593aeec1a4a
SASANO Takayoshi <uaa@uaa.org.uk>
2022-10-13 14:58:01 +0900

    revise timestamp generation in log

--- kiss.c
+++ kiss.c
@@ -248,29 +248,34 @@ static void ax25call2ether(uint8_t *addr, struct ax25callsign *c)
 	addr[5] = ul;
 }
 
-static void header_dump(struct kissheader *k, int len)
+static int timestamp_dump(char *p)
 {
-	int i;
-	char tmp[128], *p;
 	struct timeval tv;
 	struct tm *tm;
-
-	if (loglevel < 1)
-		return;
 
 	gettimeofday(&tv, NULL);
 	tm = localtime(&tv.tv_sec);
 
+	return sprintf(p, "%04d-%02d-%02d %02d:%02d:%02d.%03d ",
+		       tm->tm_year + 1900, tm->tm_mon + 1, tm->tm_mday,
+		       tm->tm_hour, tm->tm_min, tm->tm_sec,
+		       (int)(tv.tv_usec / 1000));
+}
+
+static void header_dump(struct kissheader *k, int len)
+{
+	int i;
+	char tmp[128], *p;
+
+	if (loglevel < 1)
+		return;
+
 	p = tmp;
-	p += sprintf(p, "%04d-%02d-%02d %02d:%02d:%02d.%03d ",
-		     tm->tm_year + 1900, tm->tm_mon + 1, tm->tm_mday,
-		     tm->tm_hour, tm->tm_min, tm->tm_sec,
-		     (int)(tv.tv_usec / 1000));
 
 	/* bad header: simply dump hex value */
 	if (len < sizeof(*k) || ax25_check_address_field(&k->h)) {
 		len = (len < sizeof(*k)) ? len : sizeof(*k);
-
+		p += timestamp_dump(p);
 		for (i = 0; i < len; i++)
 			p += sprintf(p, "%02X ", ((uint8_t *)k)[i]);
 		goto fin;
@@ -280,6 +285,7 @@ static void header_dump(struct kissheader *k, int len)
 		return;
 
 	/* kiss header dump */
+	p += timestamp_dump(p);
 	p += sprintf(p, "cmd=0x%02x ctl=0x%02x pid=0x%02x ",
 		     k->command, k->h.control, k->h.pid);
 	p += ax25_get_callsign(&k->h.src, p, 10);
@@ -545,7 +551,7 @@ bool ext_init(int argc, char *argv[])
 
 	return false;
 fail:
-	printf("usage: -xs[src]\n");
+	printf("usage: %s -xs[src]\n", argv[0]);
 	return true;
 }
 
